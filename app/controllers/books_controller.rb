141d25d2449183a61fc3adf58cbd41683de0b2a6
EC2 Default User <ec2-user@ip-172-31-17-166.ap-northeast-1.compute.internal>
2023-05-30 08:06:08 +0000

    edit-ok

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,20 +6,21 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     @user = current_user
-    @newbook = Book.new
+    @book = Book.new
   end
 
   def create
-
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
       #flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book.id)
     else
+      @user = current_user
       @books = Book.all
       render :index
     end
+
 
 
   end
@@ -34,6 +35,16 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+     # flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
@@ -43,6 +54,6 @@ class BooksController < ApplicationController
 
   private
   def book_params
-    params.require(:book).permit(:title, :body, :user_id)
+    params.require(:book).permit(:title, :body )
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @user = User.find(current_user.id)
+    @book = Book.new
   end
 
   def edit
@@ -13,14 +14,16 @@ class UsersController < ApplicationController
     @newbook = Book.new
     @user = User.find(params[:id])
     @books = @user.books
-
   end
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
+    if @user.update(user_params)
       #flash[:notice] = "You have updated user successfully.."
       redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
 
 
